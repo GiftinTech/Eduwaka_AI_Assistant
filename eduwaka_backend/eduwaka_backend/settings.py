b3547205434749143b4e35296dcb31981243f083
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-09-01 12:33:05 +0100

    Fix connmax 0 to prevent retries and require ssl. All for supabase session pooler.

--- eduwaka_backend/eduwaka_backend/settings.py
+++ eduwaka_backend/eduwaka_backend/settings.py
@@ -118,7 +118,8 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.config(
             default=os.environ['DATABASE_URL'],
-            conn_max_age=600
+            conn_max_age=0, 
+            ssl_require=True, 
         )
     }
 else:
